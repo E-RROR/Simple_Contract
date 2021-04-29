f0596a264a23dc0c94314b7d02286161df6409c8
Sina Farhadi <ahmadibishop@gmail.com>
2021-04-29 23:24:44 +0430

    Update Variables.sol

--- Variables.sol
+++ Variables.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16 <0.9.0;
 
-contract Variables {
+contract MyContract {
+    
+    
     
     // state variable
     uint MyUint;
@@ -12,13 +14,29 @@ contract Variables {
     bool MyBool;
     address public myAddress = 0xdfc7A8Df1f7D314c0025F762b580Be2eC050450E;
     
+    
+    
+    // arrays
+    string[] public stringArr = ["sina","test"];
+    uint[] public intArr = [1,2];
+    
+    function AddToArray(string memory _inpt) public {
+        stringArr.push(_inpt);
+    }
+    
+    function ValueCount() public view returns(uint) {
+        return stringArr.length;
+    }
+    
+    
     // struct
     struct MyStruct {
         uint number;
         string name;
     }
-    
     MyStruct public mystrct = MyStruct(1, "sina");
+    
+    
     
     // local variable
     function LocalVal() public pure returns(uint) {
